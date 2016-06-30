6b32b3ad8238ac39d7f2cbd0742fc688af9040e5
schmid <Rafael Schmid>
2016-06-30 13:16:47 -0300

    first commit

---
+++ src/config_environment.sh
@@ -0,0 +1,10 @@
+
+export CUDA_HOME=/usr/local/cuda-7.5 
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64  
+PATH=${CUDA_HOME}/bin:${PATH}
+
+export TIME=1
+export BITS=5
+export EXECS=1
+export SEGS=1048576
+#export SEGS=128
