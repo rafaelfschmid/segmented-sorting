60a434e4e984326860f30411daea303f81489e0c
Rafael Schmid <rafaelfschmid@gmail.com>
2017-09-22 18:46:34 -0400

    sequential times: fix and dummy

--- scripts/exec_all.sh
+++ scripts/exec_all.sh
@@ -5,7 +5,7 @@ out=$2 # time files dir
 #./../scripts/exec.sh radixseg.exe $in > $out/radixseg.time
 #./../scripts/exec.sh fixcub.exe $in > $out/fixcub.time
 #./../scripts/exec.sh fixthrust.exe $in > $out/fixthrust.time
-./scripts/exec.sh src/fixseq.exe $in > $out/fixseq.time
-./scripts/exec.sh src/mseq.exe $in > $out/mseq.time
+./../scripts/exec.sh fixseq.exe $in > $out/fixseq.time
+./../scripts/exec.sh mseq.exe $in > $out/mseq.time
 #./../scripts/execnthrust.sh nthrust.exe $in > $out/nthrust.time
 
