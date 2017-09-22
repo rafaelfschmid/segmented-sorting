93ef842ba358f44d3bc220a59c86fc0374df121d
Rafael Schmid <rafaelfschmid@gmail.com>
2017-09-22 09:54:45 -0400

    sequential implementations

--- scripts/exec_all.sh
+++ scripts/exec_all.sh
@@ -5,10 +5,7 @@ out=$2 # time files dir
 #./../scripts/exec.sh radixseg.exe $in > $out/radixseg.time
 #./../scripts/exec.sh fixcub.exe $in > $out/fixcub.time
 #./../scripts/exec.sh fixthrust.exe $in > $out/fixthrust.time
-./../scripts/execnthrust.sh nthrust.exe $in > $out/nthrust.time
-
-#./scripts/exec.sh src/fixmergemgpu.exe $in > $out/fixmergemgpu.time
-#/scripts/exec.sh src/fixpassdiff.exe $in > $out/fixpassdiff.time
-#./scripts/exec.sh src/fixpass.exe $in > $out/fixpass.time
-#./scripts/exec.sh src/bitonicseg/bitonicseg.exe $in > $out/bitonicseg.time
+./scripts/exec.sh src/fixseq.exe $in > $out/fixseq.time
+./scripts/exec.sh src/mseq.exe $in > $out/mseq.time
+#./../scripts/execnthrust.sh nthrust.exe $in > $out/nthrust.time
 
