3a00534ec1b567750bd9dff21cc0c78ac004bba8
Rafael Schmid <rafaelfschmid@gmail.com>
2019-02-28 16:55:16 -0400

    new sortings

---
+++ src/exec_new.sh
@@ -0,0 +1,1 @@
+/home/rafael/cuda-workspace/segmented-sorting/src/newthrust.exe < /home/rafael/cuda-workspace/inputs/segments/512_131072.in
---
+++ src/exec_old.sh
@@ -0,0 +1,1 @@
+/home/rafael/cuda-workspace/segmented-sorting/src/nthrust.exe < /home/rafael/cuda-workspace/inputs/segments/512_131072.in
