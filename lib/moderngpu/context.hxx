41692226cea2472aa2af4ce0044a5a0b26737c2c
Rafael Schmid <rafaelfschmid@gmail.com>
2019-05-09 08:49:42 -0400

    ../lib/moderngpu/context.hxx and times 1050ti mergesort

--- lib/moderngpu/context.hxx
+++ lib/moderngpu/context.hxx
@@ -47,6 +47,80 @@ struct context_t {
 // standard_context_t is a trivial implementation of context_t. Users can
 // derive this type to provide a custom allocator.
 
+class stream_context_t : public context_t {
+protected:
+  int _ptx_version;
+  cudaStream_t _stream;
+
+  cudaEvent_t _timer[2];
+  cudaEvent_t _event;
+
+public:
+  // Making this a template argument means we won't generate an instance
+  // of dummy_k for each translation unit.
+  template<int dummy_arg = 0>
+  stream_context_t(cudaStream_t s) : context_t(), _stream(s) {
+    cudaFuncAttributes attr;
+    cudaError_t error = cudaFuncGetAttributes(&attr, &dummy_k<0>);
+    _ptx_version = attr.binaryVersion;
+
+    cudaEventCreate(&_timer[0]);
+    cudaEventCreate(&_timer[1]);
+    cudaEventCreate(&_event);
+  }
+
+  ~stream_context_t() {
+    cudaEventDestroy(_timer[0]);
+    cudaEventDestroy(_timer[1]);
+    cudaEventDestroy(_event);
+  }
+
+  virtual int ptx_version() const { return _ptx_version; }
+  virtual cudaStream_t stream() { return _stream; }
+
+  // Alloc GPU memory.
+  virtual void* alloc(size_t size, memory_space_t space) {
+    void* p = nullptr;
+    if(size) {
+      cudaError_t result = (memory_space_device == space) ?
+        cudaMalloc(&p, size) :
+        cudaMallocHost(&p, size);
+      if(cudaSuccess != result) throw cuda_exception_t { result };
+    }
+    return p;
+  }
+
+  virtual void free(void* p, memory_space_t space) {
+    if(p) {
+      cudaError_t result = (memory_space_device == space) ?
+        cudaFree(p) :
+        cudaFreeHost(p);
+      if(cudaSuccess != result) throw cuda_exception_t { result };
+    }
+  }
+
+  virtual void synchronize() {
+    cudaError_t result = _stream ?
+      cudaStreamSynchronize(_stream) :
+      cudaDeviceSynchronize();
+    if(cudaSuccess != result) throw cuda_exception_t { result };
+  }
+
+  virtual cudaEvent_t event() {
+    return _event;
+  }
+  virtual void timer_begin() {
+    cudaEventRecord(_timer[0], _stream);
+  }
+  virtual double timer_end() {
+    cudaEventRecord(_timer[1], _stream);
+    cudaEventSynchronize(_timer[1]);
+    float ms;
+    cudaEventElapsedTime(&ms, _timer[0], _timer[1]);
+    return ms / 1.0e3;
+  }
+};
+
 class standard_context_t : public context_t {
 protected:
   int _ptx_version;
@@ -68,6 +142,7 @@ public:
     cudaEventCreate(&_timer[1]);
     cudaEventCreate(&_event);
   }
+
   ~standard_context_t() {
     cudaEventDestroy(_timer[0]);
     cudaEventDestroy(_timer[1]);
