4a232a20b327191d1216b43aad4866f32ab695fa
Rafael Schmid <rafaelfschmid@gmail.com>
2019-05-15 21:23:05 -0400

    newthrust

--- src/newthrust.cu
+++ src/newthrust.cu
@@ -15,9 +15,11 @@
 #include <thrust/sort.h>
 #include <thrust/copy.h>
 
-#include <future>
+//#include <future>
+#include <thread>
 #include <chrono>
 #include <iostream>
+#include <omp.h>
 
 #ifndef ELAPSED_TIME
 #define ELAPSED_TIME 0
@@ -35,9 +37,14 @@ void print(thrust::host_vector<int> h_vec) {
 	std::cout << "\n";
 }
 
-void kernelCall(cudaStream_t stream, thrust::device_vector<uint>& d_vec,
-		thrust::host_vector<uint> h_seg, int i){
-	//thrust::sort(thrust::cuda::par.on(stream),d_vec.begin() + h_seg[i], d_vec.begin() + h_seg[i + 1]);
+template<typename D, typename H>
+void kernelCall(cudaStream_t stream, D d_vec, H h_seg, int i){
+
+	cudaSetDevice(0);
+	printf("teste segment %d\n", i);
+	thrust::sort(thrust::cuda::par.on(stream),d_vec.begin() + h_seg[i], d_vec.begin() + h_seg[i + 1]);
+
+
 }
 
 int main(void) {
@@ -59,9 +66,15 @@ int main(void) {
 
 	cudaStream_t streams[NUM_STREAMS];
 	for(int i = 0; i < NUM_STREAMS; i++) {
-		//cudaStreamCreate(&streams[i]);
-		cudaStreamCreateWithFlags(&streams[i],cudaStreamNonBlocking);
+		cudaStreamCreate(&streams[i]);
+		//cudaStreamCreateWithFlags(&streams[i],cudaStreamNonBlocking);
 	}
+
+	int nstreams = NUM_STREAMS;
+	if(NUM_STREAMS > num_of_segments)
+		nstreams = num_of_segments;
+
+	omp_set_num_threads(nstreams);
 
 	for (uint i = 0; i < EXECUTIONS; i++) {
 		cudaEvent_t start, stop;
@@ -71,20 +84,13 @@ int main(void) {
 		thrust::copy(h_vec.begin(), h_vec.end(), d_vec.begin());
 
 		cudaEventRecord(start);
-		for (int i = 0; i < num_of_segments; i+=4) {
-			/*thrust::sort(thrust::cuda::par.on(streams[0]), d_vec.begin() + h_seg[i],
-					d_vec.begin() + h_seg[i + 1]);
-			thrust::sort(thrust::cuda::par.on(streams[1]), d_vec.begin() + h_seg[i+1],
-					d_vec.begin() + h_seg[i + 2]);
-
-			thrust::sort(thrust::cuda::par.on(streams[2]), d_vec.begin() + h_seg[i+2],
-					d_vec.begin() + h_seg[i + 3]);
-			thrust::sort(thrust::cuda::par.on(streams[3]), d_vec.begin() + h_seg[i+3],
-					d_vec.begin() + h_seg[i + 4]);*/
-			std::async(std::launch::async, &kernelCall,streams[0], d_vec, h_seg, i);
-			//std::async(kernelCall,streams[1], d_vec, h_seg, i+1);
-			//std::async(kernelCall,streams[2], d_vec, h_seg, i+2);
-			//std::async(kernelCall,streams[3], d_vec, h_seg, i+3);
+		#pragma omp parallel
+		{
+			uint id = omp_get_thread_num(); //cpu_thread_id
+			for (int i = 0; i < num_of_segments; i+=nstreams) {
+				uint k = i + id;
+				thrust::sort(thrust::cuda::par.on(streams[id]), d_vec.begin() + h_seg[k], d_vec.begin() + h_seg[k + 1]);
+			}
 		}
 		cudaEventRecord(stop);
 
@@ -108,3 +114,26 @@ int main(void) {
 
 	return 0;
 }
+
+
+/*thrust::sort(thrust::cuda::par.on(streams[0]), d_vec.begin() + h_seg[i],
+		d_vec.begin() + h_seg[i + 1]);
+thrust::sort(thrust::cuda::par.on(streams[1]), d_vec.begin() + h_seg[i+1],
+		d_vec.begin() + h_seg[i + 2]);
+
+thrust::sort(thrust::cuda::par.on(streams[2]), d_vec.begin() + h_seg[i+2],
+		d_vec.begin() + h_seg[i + 3]);*/
+//thrust::sort(thrust::cuda::par.on(streams[3]), d_vec.begin() + h_seg[i+3],d_vec.begin() + h_seg[i + 4]);
+//std::async(std::launch::async, &kernelCall,streams[0], d_vec, h_seg, i);
+//std::thread t1(&kernelCall<thrust::device_vector<uint>, thrust::host_vector<int>>,streams[0], d_vec, h_seg, i+0);
+//std::thread t2(&kernelCall<thrust::device_vector<uint>, thrust::host_vector<int>>,streams[1], d_vec, h_seg, i+1);
+//std::thread t3(&kernelCall<thrust::device_vector<uint>, thrust::host_vector<int>>,streams[2], d_vec, h_seg, i+2);
+//std::thread t4(&kernelCall<thrust::device_vector<uint>, thrust::host_vector<int>>,streams[3], d_vec, h_seg, i+3);
+//t1.join();
+//t2.join();
+//t3.join();
+//t4.join();
+
+//std::async(kernelCall,streams[1], d_vec, h_seg, i+1);
+//std::async(kernelCall,streams[2], d_vec, h_seg, i+2);
+//std::async(kernelCall,streams[3], d_vec, h_seg, i+3);
